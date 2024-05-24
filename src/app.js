3b933233f1bf572be5830f40598a519f0132a2b2
annahico <annahico@gmail.com>
2024-05-24 14:21:58 +0200

    feat: fixed error app.js in sequelize requiere

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 // require('dotenev').config()
 const dotenv = require('dotenv')
+const sequelize = require("./database/db.js")
 dotenv.config();
 
 const app = express()
@@ -16,6 +17,50 @@ app.get('/api/healthy', (req, res) => {
     )
 })
 
+// CRUD DE ROLE
+
+//create
+app.post("/api/roles", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Role created successfully",
+    });
+});
+
+//get all
+app.get("/api/roles", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Role retreived successfully",
+    });
+});
+
+// get by id
+app.get("/api/roles", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Author retreived successfully",
+    });
+});
+
+//update
+app.put("/api/roles", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Role updated successfully",
+    });
+});
+
+//detele
+app.delete("/api/roles", async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Role deleted successfully",
+    });
+});
+
+
+//server
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
-})
+});
