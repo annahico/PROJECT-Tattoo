7eb2591997dd0c137d954809e083d68474bcd45b
annahico <annahico@gmail.com>
2024-06-04 20:53:12 +0200

    feat: token does not work

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -148,26 +148,26 @@ authController.login = async (req, res) => {
             });
         }
 
-        // Create JWT token
-        const tokenPayload = {
-            userId: user.id,
-            userRoleName: user.role.name,
-        };
-        const token = jwt.sign(tokenPayload, process.env.JWT_SECRET_KEY, {
-            expiresIn: "3h",
-        });
+    //     // Create JWT token
+    //     const tokenPayload = {
+    //         userId: user.id,
+    //         userRoleName: user.role.name,
+    //     };
+    //     const token = jwt.sign(tokenPayload, process.env.JWT_SECRET_KEY, {
+    //         expiresIn: "3h",
+    //     });
 
-        res.status(200).json({
-            success: true,
-            message: "Login successful",
-            token,
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Login failed",
-            error: error.message,
-        });
+    //     res.status(200).json({
+    //         success: true,
+    //         message: "Login successful",
+    //         token,
+    //     });
+    // } catch (error) {
+    //     res.status(500).json({
+    //         success: false,
+    //         message: "Login failed",
+    //         error: error.message,
+    //     });
     }
 };
 
