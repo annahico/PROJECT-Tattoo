4c3b99b8a682426fe98e3a6b95f7f61f0fd2e95c
annahico <annahico@gmail.com>
2024-06-09 20:13:19 +0200

    update: createserver.js from app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const sequelize = require("./database/db.js");
 const apiRoutes = require("./database/routes");
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+// Middleware setup
+app.use(express.json());
 
-app.use(express.json()); // Middleware to parse JSON requests
-
-app.get('/api/healthy', (req, res) => {
+// Register utilities routes
+app.get("/api/healthy", (req, res) => {
     res.status(200).json({
         success: true,
-        message: "My APP server is healthy"
+        message: "My APP server is healthy",
     });
 });
 
-app.use('/api', apiRoutes); // Register API routes
+// Register API routes
+app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server listening on port: ${PORT}`);
-});
-
-sequelize.authenticate().then(() => {
-    console.log('🛢️ Database authenticated');
-}).catch((error) => {
-    console.error('Error authenticating database:', error);
-});
+module.exports = app;
