eb9400d91061f49bc0c0e8f67cc704c8dd934cc4
annahico <annahico@gmail.com>
2024-06-11 15:40:33 +0200

    feat: starting again improving the code

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/authenticationController");
-
-router.post("/register", ctrl.register);
-router.post("/login", ctrl.login);
-
-module.exports = router;
